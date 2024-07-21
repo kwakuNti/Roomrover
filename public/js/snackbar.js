cf710ad7cb2ef4dff5336de70db7aae53b2571d7
Clifford Nti Nkansah <159504184+kwakuNti@users.noreply.github.com>
2024-07-21 16:24:38 +0000

    Delete public directory

--- public/js/snackbar.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    showSnackbar();
-};
-
-function showSnackbar() {
-    var snackbar = document.getElementById("snackbar");
-    snackbar.className = "show";
-    setTimeout(function(){ snackbar.className = snackbar.className.replace("show", ""); }, 3000);
-}
