0476136ba7f37b2f42ce60602a9becef3a48232e
Clifford Nti Nkansah <159504184+kwakuNti@users.noreply.github.com>
2024-08-09 11:33:22 +0000

    Update hostel_2c.php

--- templates/hostel_2c.php
+++ templates/hostel_2c.php
@@ -15,6 +15,8 @@ include "../includes/room_selection_2c.php";
     <link rel="icon" type="image/png" sizes="32x32" href="../assets/images/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon-16x16.png">
     <link rel="manifest" href="../assets/images/site.webmanifest">
+	    <link rel="stylesheet" href="../public/css/snackbar.css">
+
 	<title>AdminSite</title>
 </head>
 <body>
